695619077629f3cc40c332065e9b09129548edaf
Bhlues <95340219+Bhlues@users.noreply.github.com>
2021-12-01 16:19:54 +0100

    1.0.2
    changed farming

--- src/main/java/features/farming.java
+++ src/main/java/features/farming.java
@@ -2,20 +2,41 @@ package features;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.event.world.WorldEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class farming {
-	
-	public boolean wart = false;
-	public boolean cane = false;
-	public boolean crops = false;
-	public boolean cocoa = false;
-	public boolean active = false;
-	public boolean activenoreset = false;
-	
+	public static boolean active = false;
+	public static boolean activenoreturn = false;
+	public static boolean farming = false;
+	public static boolean leftClick = false;
+	public static boolean rightClick = false;
+	public static boolean wart = false;
+	public static boolean cane = false;
+	public static boolean crops = false;
+
+	KeyBinding attack = Minecraft.getMinecraft().gameSettings.keyBindAttack;
+	KeyBinding place = Minecraft.getMinecraft().gameSettings.keyBindUseItem;
+	KeyBinding forward = Minecraft.getMinecraft().gameSettings.keyBindForward;
+	KeyBinding back = Minecraft.getMinecraft().gameSettings.keyBindBack;
+	KeyBinding left = Minecraft.getMinecraft().gameSettings.keyBindLeft;
+	KeyBinding right = Minecraft.getMinecraft().gameSettings.keyBindRight;
+	KeyBinding jump = Minecraft.getMinecraft().gameSettings.keyBindJump;
+
+	public static int tick = 0;
+	public static int updater = 20;
+
+	int delay = 0, errored = 0;
+	int x = 0;
+	int y = -127;
+	int z = 0;
+	int random = 5;
+	int layer = y;
+
 	private static void KeyDown(KeyBinding key) {
 		if (!key.isKeyDown())
 			KeyBinding.setKeyBindState(key.getKeyCode(), true);
@@ -26,46 +47,96 @@ public class farming {
 			KeyBinding.setKeyBindState(key.getKeyCode(), false);
 	}
 
-	KeyBinding attack = Minecraft.getMinecraft().gameSettings.keyBindAttack;
-	KeyBinding forward = Minecraft.getMinecraft().gameSettings.keyBindForward;
-	KeyBinding back = Minecraft.getMinecraft().gameSettings.keyBindBack;
-	KeyBinding right = Minecraft.getMinecraft().gameSettings.keyBindRight;
-	KeyBinding left = Minecraft.getMinecraft().gameSettings.keyBindLeft;
-	KeyBinding jump = Minecraft.getMinecraft().gameSettings.keyBindJump;
+	public static int findItemInHotbar(String name) {
+		InventoryPlayer inv = Minecraft.getMinecraft().thePlayer.inventory;
+		for (int i = 0; i < 9; i++) {
+			ItemStack curStack = inv.getStackInSlot(i);
+			if (curStack != null) {
+				if (curStack.getDisplayName().contains(name)) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+
+	public void OnTick(TickEvent.ClientTickEvent e) {
+		if (e.phase != TickEvent.Phase.START)
+			return;
+		// if (player == null)
+		//return;
+		tick++;
+		if (tick > updater) {
+			if (farming) {
+				if (wart)
+					WartFarmer();
+				if (cane)
+					CaneFarmer();
+				if (crops)
+					CropFarmer();
+			}
+			tick = 1;
+		}
+
+	}
+
+	public void WartFarmer() {
+		int tool = findItemInHotbar("Newton");
+		if (tool != -1)
+			Minecraft.getMinecraft().thePlayer.inventory.currentItem = tool;
+		// adjust angle toevoegen
+		KeyDown(attack);
+		delay++;
+		if (delay < 5)
+		return;
+		int PosY = Minecraft.getMinecraft().thePlayer.getPosition().getY();
+		if (y != PosY) {
+			KeyUp (attack);
+		}
+	}
+
+	public void CaneFarmer() {
+		KeyDown(attack);
+	}
+
+	public void CropFarmer() {
+		KeyDown(attack);
+	}
 	
-	int delay = 0, errored = 0;
-	int x = 0;
-	int y = -127;
-	int z = 0;
-	//int layer = y????
 	
-	@SubscribeEvent
+	public void CropsMovement() {
+		
+	}
+
 	public void onWorldChange(WorldEvent.Unload e) {
-		if (activenoreset)
-			Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.AQUA
-					+ "Script: " + EnumChatFormatting.RED + "Stopped due to server closing"));
-		reset();
+		if (farming)
+			Minecraft.getMinecraft().thePlayer.addChatComponentMessage(new ChatComponentText(
+					EnumChatFormatting.AQUA + "Farming paused:" + EnumChatFormatting.DARK_RED + "Due to server close"));
+		disable();
+
 	}
-	
-	/*
-	public void message (string msg) {
-		Minecraft.getMinecraft().thePlayer
-		.addChatMessage(new ChatComponentText)
-	}
-	*/
-	public void reset () {
+
+	public void disable() {
+		farming = false;
+		leftClick = false;
+		rightClick = false;
 		wart = false;
 		cane = false;
 		crops = false;
-		cocoa = false;
-		active = false;
+		x = 0;
+		y = -127;
+		z = 0;
+		layer = y;
+		delay = 0;
+		errored = 0;
 		KeyUp(attack);
+		KeyUp(place);
 		KeyUp(forward);
 		KeyUp(back);
-		KeyUp(right);
 		KeyUp(left);
-		KeyUp(jump);	
-		message (EnumChatFormatting.DARK_RED + "Turned farming off");
+		KeyUp(right);
+		KeyUp(jump);
+		// everything that needs to be turned off
 	}
-	
+
 }
