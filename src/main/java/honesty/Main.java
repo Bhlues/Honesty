06cf50ff2fd7b6830080a9094a24d13f4f4c660d
Bhlues <95340219+Bhlues@users.noreply.github.com>
2021-12-01 22:55:56 +0100

    1.0.4
    fixed 2 mods in 1 folder

--- src/main/java/honesty/Main.java
+++ src/main/java/honesty/Main.java
@@ -20,7 +20,7 @@ import utils.rotation;
 
 @Mod(modid = Main.MODID, version = Main.VERSION)
 public class Main {
-	public static final String MODID = "Apec Beta";
+	public static final String MODID = "Honesty";
 	public static final String VERSION = "1.01";
 
 	// EVENTS: MinecraftForge.EVENT_BUS.register(new Chat());
