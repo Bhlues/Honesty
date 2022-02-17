7f6ff653d34f88dc54ebef0ded2946e889e76fad
leond3 <59891470+leond3@users.noreply.github.com>
2022-02-17 18:55:41 +0100

    Update Main.java

--- src/main/java/honesty/Main.java
+++ src/main/java/honesty/Main.java
@@ -25,6 +25,8 @@ import utils.rotation;
 public class Main {
 	public static final String MODID = "Honesty";
 	public static final String VERSION = "1.19";
+	
+	// ik kan alles - spongetalisman
 
 	// EVENTS: MinecraftForge.EVENT_BUS.register(new Chat());
 	// COMMANDS: ClientCommandHandler.instance.registerCommand(new SECommand());
