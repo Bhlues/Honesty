e9522401a9d972eb0152c836018771a7fac604f1
Bhlues <95340219+Bhlues@users.noreply.github.com>
2022-03-07 10:47:05 +0100

    prof code yoinker

--- src/main/java/features/mining.java
+++ src/main/java/features/mining.java
@@ -4,128 +4,151 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.network.play.client.C07PacketPlayerDigging;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.BlockPos;
-import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.EnumChatFormatting;
-import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.*;
 import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class mining {
-    public static boolean mithril = false;
-    public static boolean jade = false;
-    public static boolean mines = false;
-    public static boolean hollows = false;
-    public static boolean mithrilmining = false;
-    public static boolean jademining = false;
-    public static boolean powder = false;
-    public static boolean ruby = false;
-    public static boolean topaz = false;
+    public static boolean pinglessMining;
+    private static BlockPos block = null;
+    private static final ArrayList<BlockPos> broken = new ArrayList<>();
+    private final KeyBinding lc = Main.mc.gameSettings.keyBindAttack;
+    private static int ticks = 0;
+    private int pinglessSpeed = 20;
 
-    KeyBinding attack = Minecraft.getMinecraft().gameSettings.keyBindAttack;
-
-    ArrayList<String> tool = new ArrayList<>(Arrays.asList("gauntlet", "x655"));
-
-    private static boolean returning = false;
-
-    private static int findItemInHotbar(String name) {
-        InventoryPlayer inv = Minecraft.getMinecraft().thePlayer.inventory;
-        for (int i = 0; i < 9; i++) {
-            ItemStack curStack = inv.getStackInSlot(i);
-            if (curStack != null) {
-                if (curStack.getDisplayName().contains(name)) {
-                    return i;
+    @SubscribeEvent
+    public void onTick80(TickEvent.PlayerTickEvent event) {
+        if (pinglessMining)
+            return;
+        if (pinglessSpeed == 0 || pinglessSpeed == 1)
+            return;
+        ticks++;
+        if (ticks % 80 == 0) {
+            broken.clear();
+        }
+        if (lc != null && lc.isKeyDown()) {
+            if (block != null) {
+                MovingObjectPosition movingObjectPosition = Main.mc.objectMouseOver;
+                if (movingObjectPosition != null
+                        && movingObjectPosition.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK) {
+                    Block b = Minecraft.getMinecraft().getBlockState(movingObjectPosition.getBlockPos()).getBlock();
+                    if (b == Blocks.stone || b == Blocks.emerald_ore || b == Blocks.lapis_ore
+                            || b == Blocks.redstone_ore ||
+                            b == Blocks.iron_ore || b == Blocks.gold_ore || b == Blocks.coal_ore
+                            || b == Blocks.diamond_ore ||
+                            b == Blocks.nether_wart || b == Blocks.reeds || b == Blocks.potatoes
+                            || b == Blocks.carrots) {
+                        broken.add(block);
+                        Main.mc.thePlayer.sendQueue.addToSendQueue(new C07PacketPlayerDigging(
+                                C07PacketPlayerDigging.Action.START_DESTROY_BLOCK, block, EnumFacing.DOWN));
+                        PlayerUtils.swingItem();
+                    }
                 }
             }
         }
-        return -1;
     }
 
-    private static void KeyDown(KeyBinding key) {
-		if (!key.isKeyDown())
-			KeyBinding.setKeyBindState(key.getKeyCode(), true);
-	}
-
-	private static void KeyUp(KeyBinding key) {
-		if (key.isKeyDown())
-			KeyBinding.setKeyBindState(key.getKeyCode(), false);
-	}
-
-    /*
-     * public void hub() {
-     * if (returning) {
-     * if (location.inSkyblock && !location.dwarven) {
-     * Thread.sleep(new Random().nextInt(15000) + 10000);
-     * Minecraft.getMinecraft().thePlayer.sendChatMessage("/warpforge");
-     * Thread.sleep(new Random().nextInt(15000) + 10000);
-     * } else if (location.dwarven) {
-     * // TODO ETHERWARPS AND TP's TO LOCATION
-     * // TODO mithril = true;
-     * }
-     * }
-     * }
-     */
-
-    public void refuel() {
-        
-    }
-
-
-    public void mithril() {
-        for (String gear : tool) {
-            if (!returning)
-                if (Minecraft.getMinecraft().thePlayer.inventory.getCurrentItem().getDisplayName().contains(gear)) {
-                    int SlotTool = findItemInHotbar(gear);
-                    if (SlotTool < 0)
-                        return;
-                    Minecraft.getMinecraft().thePlayer.inventory.currentItem = SlotTool;
-                    mithrildwarven();
-                }
+    @SubscribeEvent
+    public void onTick40(TickEvent.ClientTickEvent event) {
+        if (!pinglessMining)
+            return;
+        if (pinglessSpeed == 2)
+            return;
+        if (pinglessSpeed == 0 && event.phase == TickEvent.Phase.END)
+            return;
+        ticks++;
+        if (ticks % 80 == 0) {
+            broken.clear();
         }
-    }
-
-    public void mithrildwarven() {        
-        MovingObjectPosition objectMouseOver = Minecraft.getMinecraft().objectMouseOver;
-        if (objectMouseOver != null && objectMouseOver.typeOfHit.toString() == "wool") {
-            BlockPos pos = objectMouseOver.getBlockPos();
-            Block block = Minecraft.getMinecraft().theWorld.getBlockState(pos).getBlock();
-            if  (block == Blocks.wool) {
-                KeyDown(attack);
-            } else if (block == Blocks.prismarine) {
-                KeyDown(attack);
-            } else if (block == Blocks.bedrock) {
-                //TODO get vec3 new block
-                KeyUp(attack);
+        if (lc != null && lc.isKeyDown()) {
+            if (block != null) {
+                MovingObjectPosition movingObjectPosition = Main.mc.objectMouseOver;
+                if (movingObjectPosition != null
+                        && movingObjectPosition.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK) {
+                    Block b = Minecraft.getMinecraft().getBlockState(movingObjectPosition.getBlockPos()).getBlock();
+                    if (b == Blocks.stone || b == Blocks.emerald_ore || b == Blocks.lapis_ore
+                            || b == Blocks.redstone_ore ||
+                            b == Blocks.iron_ore || b == Blocks.gold_ore || b == Blocks.coal_ore
+                            || b == Blocks.diamond_ore) {
+                        broken.add(block);
+                        Minecraft.getMinecraft().thePlayer.sendQueue.addToSendQueue(new C07PacketPlayerDigging(
+                                C07PacketPlayerDigging.Action.START_DESTROY_BLOCK, block, EnumFacing.DOWN));
+                        PlayerUtils.swingItem();
+                    }
+                }
             }
         }
     }
-    
 
-    public void jade() {
-
+    @SubscribeEvent
+    public void onRender(RenderWorldLastEvent event) {
+        if (!pinglessMining || Minecraft.getMinecraft().thePlayer == null
+                || Minecraft.getMinecraft().theWorld == null) {
+            broken.clear();
+            return;
+        }
+        block = closestBlock(event);
+        if (block != null) {
+            RenderUtils.drawBlockBox(block, new Color(255, 0, 0), Main.configFile.lineWidth, event.partialTicks);
+        }
     }
 
-   
-
-    public void onWorldChange(WorldEvent.Unload e) {
-        if (mithrilmining) {
-            Minecraft.getMinecraft().thePlayer.addChatComponentMessage(new ChatComponentText(
-                    EnumChatFormatting.RED + "Mithril mining paused:" + EnumChatFormatting.DARK_RED
-                            + "Due to server close"));
-            returning = false;
-            mithril = false;
-        } else if (jademining) {
-            jademining = false;
-            Minecraft.getMinecraft().thePlayer.addChatComponentMessage(
-                    new ChatComponentText(EnumChatFormatting.RED + "Jade mining stopped due to server close"));
-        } else if (powder) {
-            powder = false;
-            Minecraft.getMinecraft().thePlayer.addChatComponentMessage(
-                    new ChatComponentText(EnumChatFormatting.RED + "Powder mining stopped due to server closing"));
+    private BlockPos closestBlock(RenderWorldLastEvent event) {
+        int r = 5;
+        BlockPos playerPos = Minecraft.getMinecraft().getPosition().add(0, 1, 0);
+        Vec3 playerVec = Main.mc.thePlayer.getPositionVector();
+        Vec3i vec3i = new Vec3i(r, r, r);
+        ArrayList<Vec3> blocks = new ArrayList<>();
+        for (BlockPos blockPos : BlockPos.getAllInBox(playerPos.add(vec3i), playerPos.subtract(vec3i))) {
+            IBlockState blockState = Minecraft.getMinecraft().theWorld.getBlockState(blockPos);
+            if (isLookingAtBlock(blockPos, event) && !broken.contains(blockPos)
+                    && blockState.getBlock() != Blocks.air) {
+                blocks.add(new Vec3(blockPos.getX() + 0.5, blockPos.getY(), blockPos.getZ() + 0.5));
+            }
         }
+        double smallest = 9999;
+        Vec3 closest = null;
+        for (Vec3 block : blocks) {
+            double dist = block.distanceTo(playerVec);
+            if (dist < smallest) {
+                smallest = dist;
+                closest = block;
+            }
+        }
+        if (closest != null) {
+            return new BlockPos(closest.xCoord, closest.yCoord, closest.zCoord);
+        }
+        return null;
+    }
+
+    private boolean isLookingAtBlock(BlockPos blockPos, RenderWorldLastEvent event) {
+        AxisAlignedBB aabb = AxisAlignedBB.fromBounds(blockPos.getX(), blockPos.getY(), blockPos.getZ(),
+                blockPos.getX() + 1, blockPos.getY() + 1, blockPos.getZ() + 1);
+        Vec3 position = new Vec3(Minecraft.getMinecraft().thePlayer.posX,
+                (Minecraft.getMinecraft().thePlayer.posY + Main.mc.thePlayer.getEyeHeight()), Main.mc.thePlayer.posZ);
+        Vec3 look = Minecraft.getMinecraft().thePlayer.getLook(event.partialTicks);
+        look = scaleVec(look, 0.2F);
+        for (int i = 0; i < 40; i++) {
+            if (aabb.minX <= position.xCoord && aabb.maxX >= position.xCoord && aabb.minY <= position.yCoord
+                    && aabb.maxY >= position.yCoord && aabb.minZ <= position.zCoord && aabb.maxZ >= position.zCoord) {
+                return true;
+            }
+            position = position.add(look);
+        }
+
+        return false;
+    }
+
+    private static Vec3 scaleVec(Vec3 vec, float f) {
+        return new Vec3(vec.xCoord * (double) f, vec.yCoord * (double) f, vec.zCoord * (double) f);
     }
 }
